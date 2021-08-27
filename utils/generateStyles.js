8d8d4004b488efc9ccdb543103731bc44c1ab9aa
Diana Salas <salasd10418@gmail.com>
2021-08-27 16:11:25 -0400

    transferred styles to function

---
+++ utils/generateStyles.js
@@ -0,0 +1,21 @@
+const css = () => {
+let css = `#card-container {
+    padding: 0 5%;
+}
+
+#card {
+    margin: 2%;
+}
+
+#border {
+    border: solid;
+    border-style: dashed;
+}
+
+#header {
+    font-size: 3rem;
+}`;
+return css;
+};
+
+module.exports = css;
